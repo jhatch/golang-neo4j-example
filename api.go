0624de9a037c8d4e3a0d21181e6ace55a475b04b
John Hatcher <jhatcher@jibe.com>
2014-11-04 01:24:08 -0500

    you can now hit a local neo4j instance and run a cypher via a go based REST api. braj.

--- api.go
+++ api.go
@@ -1,27 +1,58 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "log"
-    "os"
+  "fmt"
+  "net/http"
+  "net/url"
+  "bytes"
+  "log"
+  "os"
 )
 
 const PORT = "8001"
 
 func main() {
-    // mount the api
-    http.HandleFunc("/", renderWelcome)
+  // mount the api
+  http.HandleFunc("/", renderWelcome)
+  http.HandleFunc("/cypher/", runCypher)
 
-    // start 'er up
-    log.Printf("STARTING golang rest api on port %s\n", PORT)
-    err := http.ListenAndServe(":"+PORT, nil)
-    if err != nil {
-        log.Fatalf("ERROR STARTING golang rest api on port %s\n", PORT, err)
-        os.Exit(1)
-    }
+  // start 'er up
+  log.Printf("STARTING golang rest api on port %s\n", PORT)
+  err := http.ListenAndServe(":"+PORT, nil)
+  if err != nil {
+    log.Fatalf("ERROR STARTING golang rest api on port %s\n", PORT, err)
+    os.Exit(1)
+  }
 }
 
 func renderWelcome(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Welcome to the golang-neo4j-example REST api!")
+  fmt.Fprintln(w, "Welcome to the golang-neo4j-example REST api!")
 }
+
+// run a raw cypher against the local neo4j instance
+// curl http://localhost:8001/cypher/MATCH%20(all:Database)%20RETURN%20all
+func runCypher(w http.ResponseWriter, r *http.Request) {
+  uri, _ := url.Parse(r.RequestURI)
+  cypher := uri.Path[8:len(uri.Path)]
+  fmt.Fprintln(w, "Executing cypher on neo4j", cypher)
+
+  body := "{\"statements\" : [ {\"statement\" : \""+cypher+"\", \"parameters\" : {\"props\" : {\"name\" : \"My Node\"}}} ]}}"
+  b := bytes.NewBufferString(body)
+  
+  neo4jRequest, _ := http.NewRequest("POST", "http://localhost:7474/db/data/transaction", b)
+  neo4jRequest.Header.Add("Accept", "application/json; charset=UTF-8")
+  neo4jRequest.Header.Add("Content-Type", "application/json")
+
+  client := &http.Client{}
+  res, err := client.Do(neo4jRequest)
+
+  if err != nil {
+    fmt.Fprintln(w, "error running cypher", err)
+  } else {
+    buf := new(bytes.Buffer)
+    buf.ReadFrom(res.Body)
+    s := buf.String()
+    fmt.Fprintln(w, "Response:", s)
+  }
+}
+
--- server.go
+++ server.go
@@ -1,44 +1,43 @@
 package main
 
 import (
-    "net/http"
-    "log"
-    "strings"
-    "io/ioutil"
-    "os"
+  "net/http"
+  "log"
+  "strings"
+  "io/ioutil"
+  "os"
 )
 
 const PORT = "8000"
 
 func main() {
-    // serve static files
-    fs := http.FileServer(http.Dir("client"))
-    http.Handle("/", fs)
+  // serve static files
+  fs := http.FileServer(http.Dir("client"))
+  http.Handle("/", fs)
 
-    // proxy the api
-    http.HandleFunc("/api", proxyAPI)
+  // proxy the api
+  http.HandleFunc("/api", proxyAPI)
 
-    // start 'er up
-    log.Printf("STARTING golang server on port %s\n", PORT)
-    err := http.ListenAndServe(":"+PORT, nil)
-    if err != nil {
-        log.Fatalf("ERROR STARTING golang server on port %s\n", PORT, err)
-        os.Exit(1)
-    }
+  // start 'er up
+  log.Printf("STARTING golang server on port %s\n", PORT)
+  err := http.ListenAndServe(":"+PORT, nil)
+  if err != nil {
+    log.Fatalf("ERROR STARTING golang server on port %s\n", PORT, err)
+    os.Exit(1)
+  }
 }
 
 func proxyAPI(w http.ResponseWriter, r *http.Request) {
-    // http.Redirect(w, r, "http://localhost:8001" + strings.Replace(r.URL.Path, "/api", "", -1), 301)
-    response, err := http.Get("http://localhost:8001" + strings.Replace(r.URL.Path, "/api", "", -1))
+  response, err := http.Get("http://localhost:8001" + strings.Replace(r.URL.Path, "/api", "", -1))
+  if err != nil {
+    log.Fatalf("ERROR proxying to api server http://localhost:8001", err)
+  } else {
+    defer response.Body.Close()
+    contents, err := ioutil.ReadAll(response.Body)
     if err != nil {
-        log.Fatalf("ERROR proxying to api server http://localhost:8001", err)
-    } else {
-        defer response.Body.Close()
-        contents, err := ioutil.ReadAll(response.Body)
-        if err != nil {
-            log.Printf("%s", err)
-            os.Exit(1)
-        }
-        w.Write([]byte(string(contents)))
+      log.Printf("%s", err)
+      os.Exit(1)
     }
+    w.Write([]byte(string(contents)))
+  }
 }
